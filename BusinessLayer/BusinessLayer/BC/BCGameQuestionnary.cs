5bad3db63483119f5158396df1f0b7c4bef02f2f
FranckChaillat <francky.chaillat@gmail.com>
2014-05-14 17:49:12 +0200

    no message

---
+++ BusinessLayer/BusinessLayer/BC/BCGameQuestionnary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Mapping;
+
+namespace BusinessLayer.BC
+{
+    class BCGameQuestionnary : abstractBC
+    {
+        Questionnary questionnary;
+
+        public StgMsg.StgMsg setQuestionnary(DAL.DAL dal)
+        {
+            questionnary = new Questionnary("game");
+
+        }
+    }
+}
