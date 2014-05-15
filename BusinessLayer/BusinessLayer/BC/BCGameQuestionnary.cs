f544c12646b88824d3f22abcf9fdef775c7665e1
aurelierodriguez <aurelie.rodriguez28@gmail.com>
2014-05-15 08:50:18 +0200

    no message

--- BusinessLayer/BusinessLayer/BC/BCGameQuestionnary.cs
+++ BusinessLayer/BusinessLayer/BC/BCGameQuestionnary.cs
@@ -10,12 +10,25 @@ namespace BusinessLayer.BC
     class BCGameQuestionnary : abstractBC
     {
         Questionnary questionnary;
+        Player player;
 
-        public StgMsg.StgMsg setQuestionnary(DAL.DAL dal)
+        public StgMsg.StgMsg setQuestionnary(DAL.DAL dal, StgMsg.StgMsg msg)
         {
             questionnary = new Questionnary("game");
+            questionnary.setListQuestions(dal);
+
+            msg.data[1] = questionnary;
+
+            return msg;
         }
 
+        public StgMsg.StgMsg validate(StgMsg.StgMsg msg, DAL.DAL dal)
+        {
+            player = (Player)msg.data[0];
+            player.createPlayer(player, dal);
+            
+            return msg;
+        }
 
     }
 }
---
+++ client/client/CUC/CUC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.CUC
+{
+    class CUC
+    {
+        private string AppName;
+        private string pSecurity;
+        StgMsg.StgMsg msg;
+
+
+        public CUC()
+        {
+            AppName = "Ways";
+            pSecurity = "123";
+        }
+
+        public void callService(){
+            msg = new StgMsg.StgMsg();
+            msg.appName = this.AppName;
+            msg.pSecurity = this.pSecurity;
+        }
+    }
+}
